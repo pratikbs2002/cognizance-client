9086355f365e8d084ae8a8234a580a058efe4110
pratikbs2002 <20ce141@charusat.edu.in>
2023-12-03 22:32:28 +0530

    images

--- src/pages/register/ComboEvents.js
+++ src/pages/register/ComboEvents.js
@@ -1,24 +1,8 @@
 import React from "react";
 import EventCard from "./EventCard";
-import image1 from "../../assets/eventRegister/register1.jpeg";
-import image2 from "../../assets/eventRegister/register2.jpeg";
-import image3 from "../../assets/eventRegister/register3.jpeg";
-import image4 from "../../assets/eventRegister/register4.jpeg";
-import image5 from "../../assets/eventRegister/register5.jpeg";
-// import image6 from "../../assets/eventRegister/register6.jpeg";
-import image7 from "../../assets/eventRegister/register7.jpeg";
-import image8 from "../../assets/eventRegister/reg1.png";
-import image9 from "../../assets/eventRegister/coding.png";
-import image10 from "../../assets/eventRegister/flag.jpeg";
-
 import DJNight from "../../assets/eventImages/DJNight.jpg";
-import roboSoccer from "../../assets/eventImages/robo soccer.png";
-import roboRace from "../../assets/eventImages/robo race.jpg";
-import codeMania from "../../assets/eventImages/code mania.png";
-import artTech from "../../assets/eventImages/artTech.png";
-import networkinghunting from "../../assets/eventImages/network hunting.png";
 import { Card } from "@chakra-ui/react";
-// import
+
 
 const ComboEvents = () => {
     const data = [1, 2, 3, 4];
