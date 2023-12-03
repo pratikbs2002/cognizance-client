d167e19ba50bdac70c578c1ee1aae0527a24d918
pratikbs2002 <20ce141@charusat.edu.in>
2023-12-03 13:43:13 +0530

    payment Page

--- src/pages/register/Payment.js
+++ src/pages/register/Payment.js
@@ -5,16 +5,21 @@ import {
 } from "@chakra-ui/react";
 export default function Payment() {
     return (
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-            <div style={{ display: "flex", width: "100%", paddingLeft: "30px", fontWeight: "800", fontSize: "30px" }}>Payment</div>
-            <div style={{ display: "flex", width: "100%", alignItems: "center", justifyContent: "center" }}>
-                <div className='part1' style={{ display: "flex", width: "50%", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <div style={{ display: "flex", width: "100%", paddingLeft: "30px", fontWeight: "500", fontSize: "30px", paddingTop: "80px", paddingBottom: "50px" }}>Payment | ₹ 500 </div>
+            <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+                <div className='part1' style={{ borderRight: "2px solid #282b35", display: "flex", width: "50%", alignItems: "center", flexDirection: "column" }}>
                     <div className='title' style={{ fontSize: "30px", fontWeight: "400" }}>
                         Payment With Account Details
                     </div>
                     <div className='body' style={{ paddingTop: "100px", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", width: "100%" }}>
-                        <div><Text>Account Name : </Text></div>
-                        <div></div>
+                        <div style={{ width: "100%", paddingLeft: "100px", display: "flex", flexDirection: "column", gap: "10px" }}>
+                            <Text fontSize={"20px"} >Bank Name : </Text>
+                            <Text fontSize={"20px"}>Branch Name : </Text>
+                            <Text fontSize={"20px"}>IFSC Code : </Text>
+                            <Text fontSize={"20px"}>Account No : </Text>
+                            {/* <Text fontSize={"20px"}>Amount : </Text> */}
+                        </div>
                     </div>
 
                 </div>
