57ba5dfa16bb00e5eeb060c149cef4a8310eebb1
Ketan74 <20ce149@charusat.edu.in>
2023-12-04 13:35:49 +0530

    Updated details in contact us

--- src/pages/contactUs/ContactUs.js
+++ src/pages/contactUs/ContactUs.js
@@ -61,22 +61,27 @@ export default function ContactUs() {
                             }}
                         >
                             <div>
-                                <div>Mr. Harshil Patel</div>
+                                <div>Pratik Suthar</div>
                                 <div>
-                                    <a href="tel:+91 635 261 5197">
-                                        +91 635 261 5197
+                                    <a href="tel:+91 76983 62613">
+                                        +91 76983 62613
                                     </a>
                                 </div>
                             </div>
-                            <div
-                                style={{
-                                    marginLeft: "50px",
-                                }}
-                            >
-                                <div>Mr. Yash Patel</div>
+                        </div>
+                        <div
+                            style={{
+                                marginTop: "10px",
+                                fontWeight: "500",
+                                display: "flex",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <div>
+                                <div>Ketan Tiwari</div>
                                 <div>
-                                    <a href="tel:+91 635 261 5197">
-                                        +91 635 261 5197
+                                    <a href="tel:+91 70693 12561">
+                                        +91 70693 12561
                                     </a>
                                 </div>
                             </div>
@@ -106,22 +111,10 @@ export default function ContactUs() {
                             }}
                         >
                             <div>
-                                <div>Mr. Harshil Patel</div>
+                                <div>Srushti Gajjar</div>
                                 <div>
-                                    <a href="tel:+91 635 261 5197">
-                                        +91 635 261 5197
-                                    </a>
-                                </div>
-                            </div>
-                            <div
-                                style={{
-                                    marginLeft: "50px",
-                                }}
-                            >
-                                <div>Mr. Yash Patel</div>
-                                <div>
-                                    <a href="tel:+91 635 261 5197">
-                                        +91 635 261 5197
+                                    <a href="tel:+91 94095 47741">
+                                        +91 94095 47741
                                     </a>
                                 </div>
                             </div>
@@ -151,25 +144,33 @@ export default function ContactUs() {
                             }}
                         >
                             <div>
-                                <div>Mr. Harshil Patel</div>
+                                <div>Jay Shah</div>
                                 <div>
-                                    <a href="tel:+91 635 261 5197">
-                                        +91 635 261 5197
+                                    <a href="tel:+91 820007 78757">
+                                        +91 820007 78757
                                     </a>
                                 </div>
                             </div>
-                            <div
-                                style={{
-                                    marginLeft: "50px",
-                                }}
-                            >
-                                <div>Mr. Yash Patel</div>
-                                <div>
-                                    <a href="tel:+91 635 261 5197">
-                                        +91 635 261 5197
-                                    </a>
-                                </div>
-                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{
+                            color: "white",
+                            marginLeft: "20px",
+                            paddingBottom: "30px",
+                            fontSize: "20px",
+                            fontWeight: "600",
+                        }}
+                    >
+                        <div>Email</div>
+                        <div
+                            style={{
+                                fontSize: "18px",
+                            }}
+                        >
+                            <a href="cognizance@charusat.edu.in">
+                                cognizance@charusat.edu.in
+                            </a>
                         </div>
                     </div>
                 </div>
