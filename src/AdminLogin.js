4a4a5aaa764e8d0f7e26231fb575a565a0e1a228
pratikbs2002 <20ce141@charusat.edu.in>
2023-12-20 15:25:26 +0530

    private

---
+++ src/AdminLogin.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { Container, Divider, Text, useToast } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import "./pages/photoGallery/PhotoGallery.css";
+import { Box, FormControl, Input, Button, Heading } from "@chakra-ui/react";
+export default function AdminLogin(props) {
+    const toast = useToast();
+    const navigate = useNavigate();
+    const usernameEnv = process.env.REACT_APP_USER_NAME;
+    const passwordEnv = process.env.REACT_APP_USER_PASSWORD;
+
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: ""
+    });
+
+    const authenticateUser = (username, password) => {
+        if (username === usernameEnv && password === passwordEnv) {
+            props.setIsAdmin(true);
+        } else {
+            toast({
+                title: "Authentication Failed",
+                description: "Please enter a valid username and password.",
+                status: "error",
+                duration: 5000,
+                position: "top",
+                isClosable: true
+            });
+            props.setIsAdmin(false);
+        }
+    };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleLogin = () => {
+        if (credentials.username.trim() !== "" && credentials.password.trim() !== "") {
+            authenticateUser(credentials.username, credentials.password);
+        } else {
+            toast({
+                title: "Authentication Failed",
+                description: "Please enter a valid username and password.",
+                status: "error",
+                duration: 5000,
+                position: "top",
+                isClosable: true
+            });
+        }
+    };
+    return (
+        <>
+            <div className="photo-gallery-parent-container">
+                <div className="photo-gallery-navbar">
+                    <div className="photo-gallery-back-button" onClick={() => navigate(`/`)}>
+                        <IoIosArrowBack />
+                        Back
+                    </div>
+                </div>
+                <Container
+                    maxW={"100%"}
+                    width={"100%"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                >
+                    <Box
+                        maxWidth={"90%"}
+                        display={"flex"}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                        flexDirection={"column"}
+                        mt={6}
+                        padding={{ base: "20px", md: "70px" }}
+                        borderRadius={"30px"}
+                        backgroundColor={"rgba(0, 0, 0, 0.442)"}
+                    >
+                        <Heading mt={0} color={"white"} textAlign="center">
+                            Welcome
+                        </Heading>
+                        <Divider />
+                        <Box
+                            maxW={"100%"}
+                            w={"300px"}
+                            display={"flex"}
+                            alignItems={"center"}
+                            justifyContent={"center"}
+                            flexDirection={"column"}
+                            mt={10}
+                        >
+                            <FormControl>
+                                <Text color={"white"}>Username:</Text>
+                                <Input
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    value={credentials.username}
+                                    onChange={handleChange}
+                                    variant={"outline"}
+                                    textColor={"white"}
+                                />
+                            </FormControl>
+                            <FormControl mt={4}>
+                                <Text color={"white"}>Password:</Text>
+                                <Input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    value={credentials.password}
+                                    onChange={handleChange}
+                                    variant={"outline"}
+                                    textColor={"white"}
+                                />
+                            </FormControl>
+                            <Button width={"200px"} mt={6} onClick={handleLogin}>
+                                Login
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </div>
+        </>
+    );
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "react-multi-carousel/lib/styles.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import PhotoGallery from "./pages/photoGallery/PhotoGallery";
@@ -11,21 +11,26 @@ import ContactUs from "./pages/contactUs/ContactUs";
 import RegisterdEvent from "./pages/registeredEvent/RegisteredEvent";
 import NewCommittee from "./pages/committee/NewCommittee";
 import ImageGallery from "./pages/TransactionImages/GetImages";
+import AdminLogin from "./AdminLogin";
 // import Payment from "./pages/register/Payment";
 
 const App = () => {
+    const [isAdmin, setIsAdmin] = useState(false);
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/f3d0ad4e-7a64-4571-9770-b7347920ab6d" element={<UploadImages />} />
+                <Route
+                    path="/f3d0ad4e-7a64-4571-9770-b7347920ab6d"
+                    element={isAdmin ? <UploadImages /> : <AdminLogin isAdmin={isAdmin} setIsAdmin={setIsAdmin} />}
+                />
                 <Route path="/gallery" element={<PhotoGallery />} />
                 <Route path="*" element={<LandingPage />} />
                 <Route path="/contact-us" element={<ContactUs />} />
                 <Route path="/committee" element={<NewCommittee />} />
                 <Route path="/registeredEvents" element={<RegisterdEvent />} />
-                <Route path="/paymentReceipts" element={<ImageGallery />} />
+
                 {/* <Route path="newComm" element={<NewCommittee/>}/> */}
             </Routes>
         </BrowserRouter>
