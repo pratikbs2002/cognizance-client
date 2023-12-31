01a6234e2be962935ba2cc5f34dd8aaacb68b2d1
Ketan74 <20ce149@charusat.edu.in>
2024-01-01 00:04:37 +0530

    Update adModel.js with Chakra UI Modal changes

--- src/components/advertisement/adModel.js
+++ src/components/advertisement/adModel.js
@@ -1,5 +1,14 @@
 import React, { useEffect } from "react";
-import { Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton, useDisclosure, Image } from "@chakra-ui/react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Image,
+    ModalHeader
+} from "@chakra-ui/react";
 import paintball from "../../assets/adBanner/Paintball1.png";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
@@ -12,22 +21,20 @@ export default function AdModal() {
 
     return (
         <>
-            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside" size={"md"}>
+            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside">
                 <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px) hue-rotate(90deg)" />
-                <ModalContent backgroundColor={"#191818"} paddingBottom={3}>
-                    <ModalCloseButton position={"relative"} left={"90%"} />
-                    <ModalBody
-                        display={"flex"}
-                        alignItems={"center"}
-                        justifyContent={"center"}
-                        style={{ padding: "10px" }}
-                    >
+                <ModalContent backgroundColor={"#191818"} paddingBottom={3} minWidth={"min-content"}>
+                    <ModalCloseButton />
+
+                    <ModalBody alignItems={"center"} justifyContent={"center"} mt={12}>
                         <LazyLoadImage
                             alt="paintballAdvertisement"
                             effect="blur"
                             src={paintball}
                             style={{
-                                height: "72vh"
+                                minWidth: "29vw",
+                                aspectRatio: "5/7",
+                                objectFit: "cover"
                             }}
                         />
                     </ModalBody>
