482b9892c73891af2b1b23b679611bfd4b6d661e
Ketan74 <20ce149@charusat.edu.in>
2024-01-16 15:33:09 +0530

    Refactor event registration logic

--- src/pages/register/modals/EditProfile.js
+++ src/pages/register/modals/EditProfile.js
@@ -621,43 +621,46 @@ const EditProfile = (props) => {
             {props.eventName !== "Musical Night" ? (
                 !props.isAlreadyRegistered &&
                 props.maxRegistration > (typeof props?.registerCount === "undefined" ? 0 : props?.registerCount) ? (
-                    // <Button
-                    //     backgroundColor="#54cadd"
-                    //     color={"black"}
-                    //     onClick={async () => {
-                    //         if (sessionStorage.getItem("token") !== null) {
-                    //             setIsLoading(true);
-                    //             if (isProfileUpdated || (await isProfileUpdatedRequest())) {
-                    //                 // Call For Register Modal
-                    //                 // console.log("REGISTER MODAL");
-                    //                 onEventRegisterModalOpen();
-                    //                 setIsLoading(false);
-                    //             } else {
-                    //                 // Call For Profile Modal
-                    //                 if (!sessionStorage.getItem("token")) {
-                    //                     GAuth();
-                    //                     return;
-                    //                 }
-                    //                 // console.log("PROFILE MODAL");
-                    //                 onEditProfileModalOpen();
-                    //                 setIsLoading(false);
-                    //             }
-                    //         } else {
-                    //             GAuth();
-                    //         }
-                    //     }}
-                    // >
-                    //     Register
-                    // </Button>
-                    <Box
-                        alignSelf={"center"}
-                        color={"white"}
-                        backgroundColor={"green"}
-                        padding={"8px"}
-                        borderRadius={"5px"}
-                    >
-                        Registration Over!
-                    </Box>
+                    props.eventId === 30 || props.eventId === 31 ? (
+                        <Button
+                            backgroundColor="#54cadd"
+                            color={"black"}
+                            onClick={async () => {
+                                if (sessionStorage.getItem("token") !== null) {
+                                    setIsLoading(true);
+                                    if (isProfileUpdated || (await isProfileUpdatedRequest())) {
+                                        // Call For Register Modal
+                                        // console.log("REGISTER MODAL");
+                                        onEventRegisterModalOpen();
+                                        setIsLoading(false);
+                                    } else {
+                                        // Call For Profile Modal
+                                        if (!sessionStorage.getItem("token")) {
+                                            GAuth();
+                                            return;
+                                        }
+                                        // console.log("PROFILE MODAL");
+                                        onEditProfileModalOpen();
+                                        setIsLoading(false);
+                                    }
+                                } else {
+                                    GAuth();
+                                }
+                            }}
+                        >
+                            Register
+                        </Button>
+                    ) : (
+                        <Box
+                            alignSelf={"center"}
+                            color={"white"}
+                            backgroundColor={"green"}
+                            padding={"8px"}
+                            borderRadius={"5px"}
+                        >
+                            Registration Over!
+                        </Box>
+                    )
                 ) : props.isAlreadyRegistered ? (
                     <Box
                         alignSelf={"center"}
